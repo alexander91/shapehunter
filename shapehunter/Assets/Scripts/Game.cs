5ab6e29e742f610f7925589a27c2b13caa321779
alexander91 <alexander_gri@mail.ru>
2016-04-16 14:31:21 +0300

    basic scripts

---
+++ shapehunter/Assets/Scripts/Game.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game
+{
+
+    GameManager manager;
+
+    public GameManager Manager
+    {
+        get { return manager; }
+        set { manager = value; }
+    }
+
+    private static Game instance;
+    private Game()
+    { }
+    public static Game Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Game();
+            }
+            return instance;
+        }
+    }
+
+}
---
+++ shapehunter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Game.Instance.Manager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+
+
+}
