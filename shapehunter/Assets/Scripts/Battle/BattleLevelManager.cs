80a6205f775b1e443d9845c17ae6751d397aa465
alexander91 <alexander_gri@mail.ru>
2016-04-17 17:10:37 +0300

    started fight

--- shapehunter/Assets/Scripts/Battle/BattleLevelManager.cs
+++ shapehunter/Assets/Scripts/Battle/BattleLevelManager.cs
@@ -12,8 +12,20 @@ public class BattleLevelManager : MonoBehaviour {
     [SerializeField]
     UILabel endText;
 
+    [SerializeField]
+    UITweener[] changingFormTweens;
+
+    [SerializeField]
+    UITexture me;
+    [SerializeField]
+    UITexture enemy;
+
+    [SerializeField]
+    BattleManager battleManager;
 
     GameState state = new ShowingChoices();
+
+    #region States
 
     abstract class GameState
     {
@@ -28,6 +40,51 @@ public class BattleLevelManager : MonoBehaviour {
         }
     }
 
+    class ChangeFormStart : GameState
+    {
+        public override void Proceed(BattleLevelManager manager)
+        {
+            foreach (var tween in manager.changingFormTweens)
+            {
+                tween.PlayForward();
+            }
+            manager.state = new ChangeFormMiddle();
+        }
+    }
+
+    class ChangeFormMiddle : GameState
+    {
+        public override void Proceed(BattleLevelManager manager)
+        {            
+            manager.me.mainTexture = Resources.Load(manager.picturePathByAnimalName(manager.myAnimal)) as Texture2D;
+            manager.enemy.mainTexture = Resources.Load(manager.picturePathByAnimalName(manager.enemyAnimal)) as Texture2D;
+            foreach (var tween in manager.changingFormTweens)
+            {
+                tween.PlayReverse();
+            }
+
+            manager.state = new ChangeFormEnd();
+        }
+    }
+
+    class ChangeFormEnd : GameState
+    {
+        public override void Proceed(BattleLevelManager manager)
+        {
+            manager.battleManager.StartFight(manager.winDictionary[manager.myAnimal]);
+            manager.state = new Battle();
+        }
+    }
+
+    class Battle : GameState
+    {
+        public override void Proceed(BattleLevelManager manager)
+        {
+            manager.ShowFightResults(manager.myAnimal);
+            manager.state = new EndScreenChoices();
+        }
+    }
+
     class EndScreenChoices : GameState
     {
         public override void Proceed(BattleLevelManager manager)
@@ -37,13 +94,22 @@ public class BattleLevelManager : MonoBehaviour {
             Application.LoadLevel("tableScene");
         }
     }
-    
+
+    #endregion
+
     public void Proceed()
     {
         state.Proceed(this);
     }
 
     void ManageChoice(string animal)
+    {
+        myAnimal = animal;
+        state = new ChangeFormStart();
+        state.Proceed(this);
+    }
+
+    void ShowFightResults(string animal)
     {
         endText.text = winDictionary[animal].ToString();
         state = new EndScreenChoices();
@@ -57,19 +123,25 @@ public class BattleLevelManager : MonoBehaviour {
         return "art/animals/" + animal.Attributes["picture"];
     }
 
-    enum BattleResult
+    public enum BattleResult
     {
-        Win = 1,
-        Lose = -1,
-        Draw = 0
+        FightWin = 1,
+        YouRun = -1,
+        EnemyRun = 0,
+        FightLose = -2
     }
 
     Dictionary<string, BattleResult> winDictionary = new Dictionary<string, BattleResult>();
+    string enemyAnimal;
+    string myAnimal;
 
     // Use this for initialization
     void Start () {
         var targetName = Game.Instance.PlayerInfo.currentEnemy;
         target = Game.Instance.targetNode(targetName);
+
+        enemyAnimal = target.Attributes["animal"].Value;
+
         List<string> animals = new List<string>();
         var options = target.SelectSingleNode("options").SelectNodes("option");
         foreach (XmlNode animalChoice in options)
---
+++ shapehunter/Assets/Scripts/Battle/BattleManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleManager : MonoBehaviour {
+
+    public delegate void FightEnded();
+    public event FightEnded onFightEnded;
+
+    [SerializeField]
+    UITweener[] fightWin;
+    [SerializeField]
+    UITweener[] fightLose;
+    [SerializeField]
+    UITweener[] iRun;
+    [SerializeField]
+    UITweener[] enemyRun;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void RunTweens(UITweener[] tweens)
+    {
+        foreach (var tween in tweens)
+        {
+            tween.PlayForward();
+        }
+    }
+
+    public void StartFight(BattleLevelManager.BattleResult result)
+    {
+        if (result == BattleLevelManager.BattleResult.FightWin)
+        {
+            RunTweens(fightWin);
+        } else if (result == BattleLevelManager.BattleResult.FightLose)
+        {
+            RunTweens(fightLose);
+        } else if (result == BattleLevelManager.BattleResult.EnemyRun)
+        {
+            RunTweens(enemyRun);
+        } else if (result == BattleLevelManager.BattleResult.YouRun)
+        {
+            RunTweens(iRun);
+        }
+    }
+}
