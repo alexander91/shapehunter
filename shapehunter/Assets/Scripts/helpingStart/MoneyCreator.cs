2cc2711d58f26c0fb2fc578f1a6441736eef541d
alexander91 <alexander_gri@mail.ru>
2016-04-17 23:51:48 +0300

    money shower

---
+++ shapehunter/Assets/Scripts/helpingStart/MoneyCreator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoneyCreator : MonoBehaviour {
+
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start () {
+
+        var textures = GetComponentsInChildren<UITexture>();
+
+
+        for (int i = Game.Instance.PlayerInfo.money; i < textures.Length; ++i)
+        {
+            textures[i].gameObject.SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
