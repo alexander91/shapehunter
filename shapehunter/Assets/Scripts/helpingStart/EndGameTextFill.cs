cbbc39b4bf29cc80477c9d1a5859c8f410f0c9be
alexander91 <alexander_gri@mail.ru>
2016-04-19 01:37:11 +0300

    music + fly

--- shapehunter/Assets/Scripts/helpingStart/EndGameTextFill.cs
+++ shapehunter/Assets/Scripts/helpingStart/EndGameTextFill.cs
@@ -12,10 +12,10 @@ public class EndGameTextFill : MonoBehaviour {
         if (info.money > 5)
         {
             rope.SetActive(true);
-            return "You are rich and your enemies are scared of your name. But killing people you lost hope in life. You failed. \n Maybe, you should try again.";
+            return "You are rich and your enemies are scared of your name. By killing monsters you hope to defeat true monster inside you. However, each day, becoming stronger, you feel how the bludlust burns you inside out, leaving nothing human behind. Unable to fight more, you decide to put an end to it.";
         } else if (info.money > 2)
         {
-            return "Your career is over. You earned enough money for comfortable existence. \n Now you have a hope to find a long-awaited peace. \n And only at the full moon bloodlust again and again will wake up. \n This is your fate.Your battle. Until the end of your days.";
+            return "Your career is over. You earned enough money for comfortable existence. \n Now you have a hope to find a long-awaited peace. \n And only at the full moon bloodlust again and again will wake up. \n This is your fate.Your battle. Until the end of your days. \n This might not be the maximum you could achieve.";
         } else 
         {
             return "Enough! You decide that you have had enough of this, time to make your life peaceful! You can say you won.";
---
+++ shapehunter/Assets/Scripts/helpingStart/MusicPicker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPicker : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        var audios = GetComponents<AudioSource>();
+        foreach (var a in audios)
+        {
+            a.Stop();
+        }
+        audios[Random.Range(0, audios.Length)].Play();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
